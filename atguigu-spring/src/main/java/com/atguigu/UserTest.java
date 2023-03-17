b8aa00438f5f938819d8a4f7bcbb8db4622051c5
RoselleShaw <a2548571483@gmail.com>
2023-03-17 11:06:16 +0800

    hot-fix commit2

--- atguigu-spring/src/main/java/com/atguigu/UserTest.java
+++ atguigu-spring/src/main/java/com/atguigu/UserTest.java
@@ -18,6 +18,7 @@ public class UserTest {
         userService.add();
         System.out.println("hello git!");
         System.out.println("hello git2");
+        System.out.println("hot-fix test");
     }
 
 }
